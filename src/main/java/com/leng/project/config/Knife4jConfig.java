3ab3134f85b47be6aad14602b472fb425765d90b
xiaoleng <1873048956@qq.com>
2025-05-12 11:38:10 +0800

    doc:优化项目文档

--- src/main/java/com/leng/project/config/Knife4jConfig.java
+++ src/main/java/com/leng/project/config/Knife4jConfig.java
@@ -1,18 +1,24 @@
 package com.leng.project.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Knife4j接口文档配置
+ */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@EnableKnife4j
+public class Knife4jConfig {
 
     @Bean
     public Docket defaultApi() {
@@ -30,6 +36,10 @@ public class SwaggerConfig {
                 .title("API接口开放平台接口文档")
                 .version("1.0")
                 .description("API 接口服务平台是一个为用户和开发者提供全面 API 接口调用服务的平台")
+                .termsOfServiceUrl("http://localhost:8101/api/doc.html")
+                .contact(new Contact("leng",
+                        "https://github.com/xiaoleng-ros/lengapi-backend ",
+                        "1873048956@qq.com"))
                 .build();
     }
 }
